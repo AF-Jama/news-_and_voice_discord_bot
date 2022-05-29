be08b17aec11aaeb2f61a86967d3580b6f55fca2
AF-Jama <Abdifatah.jama01@protonmail.com>
2022-05-29 23:04:28 +0100

    Added news feature where BBC news articles are populated into the news channel using cron jobs. Each time, the previous news article in the channel is checked for it's title and if it's not there it is posted in the channel

--- bot.js
+++ bot.js
@@ -4,18 +4,20 @@ import {Routes} from 'discord-api-types/v9'
 import 'dotenv/config'
 import {readBannedWordFile} from './utils.js' 
 import { commands,CreateInteraction } from './commands/commands.js'
-import { getBreakingNews } from './news/news.js'
+import { getBreakingNews,getLastMessageInNewsChannel,postNewsArticle } from './news/news.js'
 import { fs, rmdirSync } from 'file-system'
-import { channelMention, SlashCommandBuilder } from '@discordjs/builders'
+import { channelMention, createComponentBuilder, SlashCommandBuilder } from '@discordjs/builders'
 import { VoiceConnection,AudioPlayerStatus, VoiceConnectionStatus } from '@discordjs/voice'
 import fetch from 'node-fetch'
+import * as cron from 'node-schedule'
 import { ConnectionVisibility } from 'discord-api-types/v10'
 
 const prefix = '!'
 
+
+
 const newsApiKey = process.env.NEWS_API_KEY // constant authorisation API key that is used to make requests to news api
-const URL = `https://newsdata.io/api/1/news?apikey=${newsApiKey}&country=gb,us&category=world,entertainment,politics,technology,environment&language=en&page=1` // BASE URL TO REQUEST TO
-const UK_URL = `https://newsapi.org/v2/top-headlines?country=gb&apiKey=${newsApiKey}`
+const URL = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${newsApiKey}`
 const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES,Intents.FLAGS.DIRECT_MESSAGES] }) //creates clients object with intents
 
 // need to create function that reads the banned-word.txt file which can then be used to prevent messages containing banned words
@@ -55,16 +57,25 @@ client.on('messageCreate',async (msg)=>{
         msg.guild.channels.create(name, {
             type: 'GUILD_TEXT',
     })
-    .then(res=>console.log(res))
-    .catch(err=>console.log(err))
-}
+
+
+    }
+
+    if(msg.channel.name === 'news-channel' && msg.author.bot !=true){
+        msg.delete()
+        .then(res=>console.log(`Succesfully deleted: ${res}`))
+        .catch(err=>console.log(err))
+    }
 
 })
 
-// client.on("messageDelete",msg=>{
-
+// client.on('ready',async ()=>{
+//     const news_channel_id = process.env.NEWS_CHANNEL_ID
+//     client.channels.fetch(news_channel_id)
+//     const res = await getBreakingNews(URL)
+//     const text = res.status
+//     client.channels.fetch(news_channel_id).then(channel=>channel.send("SENT TO NEWS CHANNEL"))
 // })
-
 
 client.once('ready',()=>{
     console.log("Bot has woken up to play")
@@ -83,3 +94,19 @@ client.on(VoiceConnectionStatus.Ready,(oldState,newState)=>{
     console.log("Connection is in the ready state!")
 })
 
+cron.scheduleJob('*/20 * * * *',async ()=>{
+    try {
+        let res = await getBreakingNews(URL) // return response
+        res = res.articles // returns array of news objects
+        res = res[0] // returns news object as index 0 -> ie: most recent news article
+        let lastMessage = await getLastMessageInNewsChannel(client,process.env.NEWS_CHANNEL_ID)
+        lastMessage = lastMessage.split('\n')[0]
+        // compare 
+        if(!(lastMessage === res.title)) await postNewsArticle(client,process.env.NEWS_CHANNEL_ID,res)
+        console.log("Nope")
+    } catch (error) {
+        console.log(`There was an error during the execution of the job: ${error}`)
+    }
+})
+
+// setTimeout(()=>getLastMessageInNewsChannel(client,'979797391353991188'),3000)
--- news/news.js
+++ news/news.js
@@ -1,6 +1,44 @@
+import fetch from "node-fetch";
+import * as c from 'node-schedule'
+
+
 export const getBreakingNews = async(baseURL)=>{
-    let res = await fetch(baseURL) // returns response value in returned promise
-    res = await res.json() //returns values from returned json promise
-    
-    return res; // returns value as a promise
+    try{
+        let res = await fetch(baseURL) // returns response value in returned promise
+        res = await res.json() //returns values from returned json promise
+        return res; // returns value as a promise
+    }catch(e){
+        console.log(`There was an error ${e}`)
+    }
 }
+
+// const x = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=fc37270c8c2e4359aa876d56a659a346'
+
+// getBreakingNews(`https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=fc37270c8c2e4359aa876d56a659a346`).then(res=>console.log(res))
+
+// getBreakingNews('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=fc37270c8c2e4359aa876d56a659a346')
+
+export const getLastMessageInNewsChannel = async (client,channelId) =>{
+    try {
+        const channel = await client.channels.fetch(channelId) // returns promise value, in this case this returns the fetched channel
+        let message = await channel.messages.fetch({ limit: 1 })
+        return message.at(0).content        
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const deleteMessagesInNewsChannel = ()=>{
+    //deletes message that is not sent by bot
+
+}
+
+export const postNewsArticle = async (client,channelId,res)=>{
+    try{
+        const channel = await client.channels.fetch(channelId)
+        await channel.send({content: `${res.title}\n${res.url}` })
+        return ;
+    }catch(err){
+        console.log(err)
+    }
+} 
